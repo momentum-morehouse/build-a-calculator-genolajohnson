9d12ec026ac71d9281e90a4bb10f408d8e294de6
Genola Johnson <genolajohnson@gmail.com>
2020-06-10 19:27:03 +0000

    Began js

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+class Calculator {
+    constructor(previousOperandTextElenent, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+clear() {
+
+}
+
+delete () {
+
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+computer() {
+
+}
+
+updateDisplay () [
+    
+]
+
+const numberButtons = document.querySelectorAll("[data number]")
+const operationButtons = document.querySelectorAll("data-operation"])
+const equalsbutton = document.querySelector("[data-equals]")
+const deletebutton = document.querySelector("[data-delete]")
+const allClearsbutton = document.querySelector("[data-all-clear]")
+const previousOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand]")
+
 
